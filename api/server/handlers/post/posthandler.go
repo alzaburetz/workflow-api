472188b6536b2d96b73b44e9d09fb9ef2900d427
Alexey Zaburetz <anton.motin.dev@gmail.com>
2020-02-05 23:56:29 +0300

    Post functionality added

---
+++ api/server/handlers/post/posthandler.go
@@ -0,0 +1,22 @@
+package post
+
+import (. "app/server/handlers/user"
+		"errors")
+
+type Post struct {
+	Id int `json:"id" bson:"_id_:`
+	Author *User `json:"author" bson:"author"`
+	Name string `json:"name" bson:"name"`
+	Body string `json:"body" bson:"body"`
+	Timestamp int64 `json:"timestamp" bson:"timestamp"`
+}
+
+func (p Post)HasRequiredFields() error  {
+	if len(p.Name) == 0 {
+		return errors.New("Post name is empty")
+	} else if len(p.Body) == 0 {
+		return errors.New("Post body is empty")
+	}
+
+	return nil
+}
