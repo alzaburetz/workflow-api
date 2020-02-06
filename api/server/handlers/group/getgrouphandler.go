edc66ab5e872acbc7276c1e2bf556df90d6b7838
Alexey Zaburetz <anton.motin.dev@gmail.com>
2020-02-06 23:31:32 +0300

    Get group handler added

---
+++ api/server/handlers/group/getgrouphandler.go
@@ -0,0 +1,35 @@
+package group
+
+import ("net/http"
+		. "app/server/handlers"
+		"gopkg.in/mgo.v2/bson"
+		"strconv"
+		"github.com/gorilla/mux")
+
+func GetGroup(w http.ResponseWriter, r *http.Request) {
+	var muxvar = mux.Vars(r)
+	var idurl string
+	var err error
+	var id int
+	var converted bool
+	if idurl, converted = muxvar["id"]; converted != true {
+		w.WriteHeader(http.StatusBadRequest)
+		WriteAnswer(&w, nil, []string{"ID of group not provided", "Usage: /group/{id}"}, 400)
+		return
+	}
+	if id, err = strconv.Atoi(idurl); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		WriteAnswer(&w, nil, []string{ err.Error() }, 500)
+		return
+	}
+	var group Group
+	var database = AccessDataStore()
+	defer database.Close()
+	if err := database.DB("app").C("Groups").Find(bson.M{"_id_":id}).One(&group); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		WriteAnswer(&w, nil, []string{"Database error", "Error getting group from database", err.Error()}, 500)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	WriteAnswer(&w, group, []string{}, 200)
+}
