4e8f4e287a22a40ff4eaba640a3edb56a501fb92
Alexey Zaburetz <anton.motin.dev@gmail.com>
2020-02-09 01:18:50 +0300

    Group delete by string uuid

--- api/server/handlers/group/deletegrouphandler.go
+++ api/server/handlers/group/deletegrouphandler.go
@@ -3,7 +3,6 @@ package group
 import ("net/http"
 		. "app/server/handlers"
 		// . "app/server/handlers/user"
-		"strconv"
 		. "app/server/middleware"
 		"gopkg.in/mgo.v2/bson"
 		"github.com/gorilla/mux")
@@ -14,13 +13,6 @@ func DeleteGroup(w http.ResponseWriter, r *http.Request) {
 	if !parsed {
 		w.WriteHeader(http.StatusBadRequest)
 		WriteAnswer(&w, nil, []string{"Error getting id from url", "Usage: /group/{id}/delete"}, 400)
-		return
-	}
-
-	id, err := strconv.Atoi(groupid)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		WriteAnswer(&w, nil, []string{"Error getting id", err.Error()}, 400)
 		return
 	}
 
@@ -36,7 +28,7 @@ func DeleteGroup(w http.ResponseWriter, r *http.Request) {
 
 	var group Group
 
-	iter := database.DB("app").C("Groups").Pipe([]bson.M{{"$match": bson.M{"$and": []bson.M{{"creator.email":email}, {"_id_":id}}}}}).Iter()
+	iter := database.DB("app").C("Groups").Pipe([]bson.M{{"$match": bson.M{"$and": []bson.M{{"creator.email":email}, {"_id_":groupid}}}}}).Iter()
 	var groupcount int 
 	for iter.Next(&group) {
 		groupcount++
@@ -46,14 +38,14 @@ func DeleteGroup(w http.ResponseWriter, r *http.Request) {
 		WriteAnswer(&w,nil,[]string{"You are not the owner"},401)
 		return
 	}
-	_, err = database.DB("app").C("Users").UpdateAll(bson.M{"_id_":bson.M{"$gte":0}}, bson.M{"$pull":bson.M{"groups":id}})
+	_, err = database.DB("app").C("Users").UpdateAll(bson.M{"_id_":bson.M{"$gte":0}}, bson.M{"$pull":bson.M{"groups":groupid}})
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		WriteAnswer(&w, nil, []string{err.Error()}, 500)
 		return
 	}
 
-	if err = database.DB("app").C("Groups").Remove(bson.M{"_id_":id}); err != nil {
+	if err = database.DB("app").C("Groups").Remove(bson.M{"_id_":groupid}); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		WriteAnswer(&w, nil, []string{"Error removing group", err.Error()}, 500)
 		return
