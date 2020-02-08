8008ba2664c2e582027d696883b37db8a57c7ede
Alexey Zaburetz <anton.motin.dev@gmail.com>
2020-02-09 01:33:17 +0300

    Check if user deleting post is the owner

--- api/server/handlers/post/deletepost.go
+++ api/server/handlers/post/deletepost.go
@@ -1,40 +1,54 @@
 package post
 
 import ("net/http"
-		"strconv"
 		"gopkg.in/mgo.v2/bson"
 		. "app/server/handlers"
+		. "app/server/middleware"
 		"github.com/gorilla/mux")
 
 func DeletePost(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	group, _ := vars["id"]
 	post, _ := vars["post"]
-	groupid, err := strconv.Atoi(group)
-	postid, err :=strconv.Atoi(post)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		WriteAnswer(&w, nil, []string{"Error reading id", "Usage: /group/{id}/posts/{post}/delete", err.Error()}, 400)
-		return
-	}
 
 	var database = AccessDataStore()
 	defer database.Close()
 
 	var post_ Post
 
-	post_id_group := []bson.M{bson.M{"group_id":groupid},bson.M{"_id_":postid}}
+	post_id_group := []bson.M{bson.M{"group_id":group},bson.M{"_id_":post}}
 
 	var and_operator = bson.M{"$and":post_id_group}
 
-	var match = bson.M{"$match":and_operator}
+	//var match = bson.M{"$match":and_operator}
 
-	if err = database.DB("app").C("Posts").Find(match).One(&post_); err != nil {
+	if err := database.DB("app").C("Posts").Find(and_operator).One(&post_); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		WriteAnswer(&w, nil, []string{"Error getting post from database", err.Error()}, 500)
 		return
 	}
 
+	err, email := CheckToken(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		WriteAnswer(&w, nil, []string{"Error checking token", err.Error()}, 500)
+		return
+	}
+
+	if post_.Author.Email != email {
+		w.WriteHeader(http.StatusUnauthorized)
+		WriteAnswer(&w, nil, []string{"You are not the owner of the post"}, 401)
+		return
+	} else {
+		if err = database.DB("app").C("Posts").Remove(and_operator); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			WriteAnswer(&w, nil, []string{"Database error", err.Error()}, 500)
+			return
+		}
+	}
+
+	
+
 	w.WriteHeader(http.StatusOK)
-	WriteAnswer(&w, post_, []string{}, 200)
+	WriteAnswer(&w, "Successfully removed post", []string{}, 200)
 }
