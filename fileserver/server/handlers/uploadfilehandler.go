6b148ee526d34d873792736acbd05f0c4eb27c14
Alexey Zaburetz <anton.motin.dev@gmail.com>
2020-02-10 22:19:59 +0300

    Handler for uplading file

---
+++ fileserver/server/handlers/uploadfilehandler.go
@@ -0,0 +1,34 @@
+package handlers
+
+import ("net/http"
+		"fmt"
+		"os"
+		"io"
+		"time")
+		// "gopkg.in/mgo.v2/bson"
+		// . "fileserver/server")
+
+func UploadFile(w http.ResponseWriter, r *http.Request) {
+	user := r.Header.Get("User")
+	if len(user) == 0 {
+		fmt.Fprintf(w, "No user in handler")
+		return
+	}
+
+	r.ParseMultipartForm(10 << 20)
+	file, _, err := r.FormFile("File")
+	if err != nil {
+		return
+	} 
+	defer file.Close()
+	fileName := user + string(time.Now().Unix())
+	_ = os.Mkdir("./static", 0777)
+	f, err := os.OpenFile("./static/" + fileName + ".png", os.O_WRONLY | os.O_CREATE, 0666)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer f.Close()
+	io.Copy(f, file)
+	
+}
