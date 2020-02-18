757a0bbe920cedb4c4f15c906297aba33d778019
alzaburetz <anton.motin.dev@gmail.com>
2020-02-18 23:31:38 +0300

    Comment entity created

---
+++ api/server/handlers/comment/commententity.go
@@ -0,0 +1,17 @@
+package comment
+
+import  . "github.com/alzaburetz/workflow-api/api/server/handlers/user"
+
+type Comment struct {
+	Uid string `json:"id" bson:"_id_"`
+	PostId string `json:"postid" bson:"postid"`
+	Author User `json:"author" bson:"author"`
+	At User `json:"at" bson:"at"`
+	Body string `json:"body" bson:"body"`
+	Created int64 `json:"created" bson:"created"`
+	Likes int `json:"likes" bson:"likes"`
+}
+
+func (c *Comment)BodyNotEmpty() bool {
+	return len(c.Body) > 0
+}
