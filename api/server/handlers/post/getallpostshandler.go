da3d2e10b481b97d80e56a3a077a30710860dc1d
alzaburetz <anton.motin.dev@gmail.com>
2020-02-26 19:27:56 +0300

    Method covers likes

--- api/server/handlers/post/getallpostshandler.go
+++ api/server/handlers/post/getallpostshandler.go
@@ -2,6 +2,8 @@ package post
 
 import (
 	. "github.com/alzaburetz/workflow-api/api/server/handlers"
+	"github.com/alzaburetz/workflow-api/api/server/middleware"
+	"github.com/alzaburetz/workflow-api/api/util"
 	"github.com/gorilla/mux"
 	"gopkg.in/mgo.v2/bson"
 	"net/http"
@@ -20,6 +22,13 @@ func GetAllPosts(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		WriteAnswer(&w, nil, []string{"Error getting data from database", err.Error()}, 500)
 		return
+	}
+
+	_, user := middleware.CheckToken(r)
+
+	for key,val := range posts {
+		posts[key].LikesCount = len(val.Likes)
+		posts[key].LikedByUser = util.Contains(val.Likes, user)
 	}
 
 	w.WriteHeader(http.StatusOK)
