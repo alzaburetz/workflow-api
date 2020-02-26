03938fcde58dbe7a3774a7490c88a281bdec0175
alzaburetz <anton.motin.dev@gmail.com>
2020-02-26 19:23:52 +0300

    Comment entity added

--- api/server/handlers/comment/commententity.go
+++ api/server/handlers/comment/commententity.go
@@ -1,17 +1,18 @@
 package comment
 
-import  . "github.com/alzaburetz/workflow-api/api/server/handlers/user"
+import . "github.com/alzaburetz/workflow-api/api/server/handlers/user"
 
 type Comment struct {
-	Uid string `json:"id" bson:"_id_"`
-	PostId string `json:"postid" bson:"postid"`
-	Author User `json:"author" bson:"author"`
-	At User `json:"at" bson:"at"`
-	Body string `json:"body" bson:"body"`
-	Created int64 `json:"created" bson:"created"`
-	Likes int `json:"likes" bson:"likes"`
+	Uid     string `json:"id" bson:"_id_"`
+	PostId  string `json:"postid" bson:"postid"`
+	Author  User   `json:"author" bson:"author"`
+	At      User   `json:"at" bson:"at"`
+	Body    string `json:"body" bson:"body"`
+	Created int64  `json:"created" bson:"created"`
+	Edited  int64  `json:"edited" bson:"edited"`
+	Likes   int    `json:"likes" bson:"likes"`
 }
 
-func (c *Comment)BodyNotEmpty() bool {
+func (c *Comment) BodyNotEmpty() bool {
 	return len(c.Body) > 0
 }
