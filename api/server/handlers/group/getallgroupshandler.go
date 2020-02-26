bfa93906b12094fef991cf87951fccc443ce6225
alzaburetz <anton.motin.dev@gmail.com>
2020-02-26 19:25:29 +0300

    Search functionality added

--- api/server/handlers/group/getallgroupshandler.go
+++ api/server/handlers/group/getallgroupshandler.go
@@ -2,20 +2,25 @@ package group
 
 import (
 	. "github.com/alzaburetz/workflow-api/api/server/handlers"
+	"gopkg.in/mgo.v2/bson"
 	"net/http"
 )
 
 func GetAllGroups(w http.ResponseWriter, r *http.Request) {
 	var groups []Group
 
+	query, ok := r.URL.Query()["search"]
+	var filter bson.M
+	if ok {
+		filter = bson.M{"name": bson.M{"$regex": ".*" + string(query[0]) + ".*"}}
+	} else {
+		filter = nil
+	}
+
 	var database = AccessDataStore()
 	defer database.Close()
 
-	if err := database.DB(DBNAME).C("Groups").Find(nil).All(&groups); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		WriteAnswer(&w, nil, []string{"Error fetching data from database", err.Error()}, 500)
-		return
-	}
+	database.DB(DBNAME).C("Groups").Find(filter).All(&groups)
 
 	w.WriteHeader(http.StatusOK)
 	WriteAnswer(&w, groups, []string{}, 200)
