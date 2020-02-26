05bf8b6200814cc0b2880dafbb861236e962cf7c
alzaburetz <anton.motin.dev@gmail.com>
2020-02-26 19:29:19 +0300

    Post entity now has like and comment fields

--- api/server/handlers/post/posthandler.go
+++ api/server/handlers/post/posthandler.go
@@ -2,6 +2,7 @@ package post
 
 import (
 	"errors"
+	"github.com/alzaburetz/workflow-api/api/server/handlers/comment"
 	. "github.com/alzaburetz/workflow-api/api/server/handlers/user"
 )
 
@@ -12,6 +13,12 @@ type Post struct {
 	Name      string `json:"name" bson:"name"`
 	Body      string `json:"body" bson:"body"`
 	Timestamp int64  `json:"timestamp" bson:"timestamp"`
+	Comments []comment.Comment `json:"comments" bson:"-"`
+	CommentsCount int `json:"comments_count" bson:"comments"`
+	LikesCount int `json:"likes" bson:"likescount"`
+	LikedByUser bool `json:"liked" bson:"-"`
+	Likes []string `json:"-" bson:"likes"`
+	Tags []string `json:"tags" bson:"tags"`
 }
 
 func (p Post) HasRequiredFields() error {
